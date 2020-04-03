700bf06007e4159eee0ffa659ed480f0499bfb82
Maria Vodolazkaya <vodolazkayamv@gmail.com>
2020-04-04 00:05:37 +0300

    fixed .gitignore

--- Pods/Target Support Files/Pods-covid-info-covid-infoUITests/Pods-covid-info-covid-infoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_covid_info_covid_infoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_covid_info_covid_infoUITests
-@end
--- Pods/Target Support Files/Pods-covid-info/Pods-covid-info-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_covid_info : NSObject
-@end
-@implementation PodsDummy_Pods_covid_info
-@end
